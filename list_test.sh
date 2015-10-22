bcd2fdd9819ed7206cd01a3da23ef4901efc62ac
mfm92 <mfmoser92@gmail.com>
2015-10-22 15:43:29 +0200

    letting multiple processes terminate more gracefully

--- list_test.sh
+++
@@ -1,8 +0,0 @@
-clang -w -g -m32 -D'main(a, b)=main(int argc, char **argv)' -o selfie selfie.c
-touch out
-./selfie < selfie.c
-mv out selfie.mips1
-touch out
-./selfie < test.c
-mv out myprog
-./selfie -m 32 myprog
