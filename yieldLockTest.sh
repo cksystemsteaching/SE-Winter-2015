4a99c6b0c0ab9c3ee056a90cf8b5aa38542e32aa
Lukas Buchner <lukas.buchner@stud.sbg.ac.at>
2015-11-05 22:34:11 +0100

    rename executed file

--- yieldLockTest.sh
+++ yieldLockTest.sh
@@ -1,4 +1,4 @@
 gcc -o selfie selfie.c
-touch yieldTest1.mips
-./selfie -c yieldTest1.c -o yieldTest1.mips
-./selfie -l yieldTest1.mips -m 32 yieldTest1.mips
+touch yieldLockTest.mips
+./selfie -c yieldLockTest.c -o yieldLockTest.mips
+./selfie -l yieldLockTest.mips -m 32 yieldLockTest.mips
