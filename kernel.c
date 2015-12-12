db200505636ac88313731df3777deef9b3b2e4d9
Christian Kawalar <christian@kawalar.net>
2015-12-12 16:26:03 +0100

    Kernel.c added

---
+++ kernel.c
@@ -0,0 +1,10 @@
+//========================================
+//Team USEG Microkernel Server
+//Kawalar Christian
+//Schlager Daniel
+//Mayer Alexander
+//========================================
+
+int main(){
+
+}
